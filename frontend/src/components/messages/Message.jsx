ed60aa80b9bb9e7d30f3653828bd029d03277017
Diptangshu Ghosh <dippghosh7@gmail.com>
2024-07-13 02:12:18 +0530

    backend integration

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,17 +1,31 @@
-const Message = () => {
+import { useAuthContext } from "../../context/AuthContext";
+import { extractTime } from "../../utils/extractTime";
+import useConversation from "../../zustand/useConversation";
+
+const Message = ({ message }) => {
+  const { authUser } = useAuthContext();
+  const { selectedConversation } = useConversation();
+
+  const fromMe = message.senderId === authUser._id;
+  const chatClassName = fromMe ? "chat-end" : "chat-start";
+  const profilePic = fromMe
+    ? authUser.profilePic
+    : selectedConversation?.profilePic;
+  const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+  const formattedTime = extractTime(message.createdAt);
+
   return (
-    <div className="chat chat-end">
+    <div className={`chat ${chatClassName}`}>
       <div className="chat-image avatar">
         <div className="w-10 rounded-full">
-          <img
-            alt="chat user avatar"
-            src="https://static.vecteezy.com/system/resources/previews/004/511/281/original/default-avatar-photo-placeholder-profile-picture-vector.jpg"
-          />
+          <img alt="chat user avatar" src={profilePic} />
         </div>
       </div>
-      <div className="chat-bubble text-white bg-blue-500">Hi! How are you?</div>
+      <div className={`chat-bubble text-white ${bubbleBgColor}`}>
+        {message.message}
+      </div>
       <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
-        12:42
+        {formattedTime}
       </div>
     </div>
   );
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,6 +1,27 @@
+import { Link } from "react-router-dom";
 import GenderCheckbox from "./GenderCheckbox";
+import { useState } from "react";
+import useSignup from "../../hooks/useSignup";
 
 const SignUp = () => {
+  const [input, setInput] = useState({
+    fullName: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+    gender: "",
+  });
+
+  const { loading, signup } = useSignup();
+
+  const handleCheckboxChange = (gender) => {
+    setInput({ ...input, gender });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await signup(input);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
       <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
@@ -8,7 +29,7 @@ const SignUp = () => {
           Sign up for <span className="text-blue-500">ChatApp</span>
         </h1>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label className="label p-2">
               <span className="text-base label-text">Full Name</span>
@@ -17,6 +38,10 @@ const SignUp = () => {
               type="text"
               placeholder="John Doe"
               className="w-full input input-bordered h-10"
+              value={input.fullName}
+              onChange={(e) => {
+                setInput({ ...input, fullName: e.target.value });
+              }}
             />
           </div>
           <div>
@@ -27,6 +52,10 @@ const SignUp = () => {
               type="text"
               placeholder="johndoe"
               className="w-full input input-bordered h-10"
+              value={input.username}
+              onChange={(e) => {
+                setInput({ ...input, username: e.target.value });
+              }}
             />
           </div>
           <div>
@@ -37,6 +66,10 @@ const SignUp = () => {
               type="password"
               placeholder="Enter Password"
               className="w-full input input-bordered h-10"
+              value={input.password}
+              onChange={(e) => {
+                setInput({ ...input, password: e.target.value });
+              }}
             />
           </div>
           <div>
@@ -47,18 +80,32 @@ const SignUp = () => {
               type="password"
               placeholder="Confirm Password"
               className="w-full input input-bordered h-10"
+              value={input.confirmPassword}
+              onChange={(e) => {
+                setInput({ ...input, confirmPassword: e.target.value });
+              }}
             />
           </div>
-          <GenderCheckbox />
-          <a
-            href="#"
+          <GenderCheckbox
+            onCheckbocChange={handleCheckboxChange}
+            selectedGender={input.gender}
+          />
+          <Link
+            to="/login"
             className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block"
           >
             Already have an account?
-          </a>
+          </Link>
           <div>
-            <button className="btn btn-block btn-sm mt-2 border border-slate-700">
-              Sign up
+            <button
+              className="btn btn-block btn-sm mt-2 border border-slate-700"
+              disabled={loading}
+            >
+              {loading ? (
+                <span className="loading loading-spinner"></span>
+              ) : (
+                "Sign up"
+              )}
             </button>
           </div>
         </form>
